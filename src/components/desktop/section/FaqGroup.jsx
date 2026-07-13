5da3f53e4918773964305eb1487683d708361cea
Pavel <expodevs@gmail.com>
2026-07-13 20:54:42 +0300

    fix click faq

--- src/components/desktop/section/FaqGroup.jsx
+++ src/components/desktop/section/FaqGroup.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './FaqGroup.module.scss'
 
-
 /** @typedef {import('@app/api/front/page').FaqItem} FaqItem */
 
 /**
@@ -17,10 +16,13 @@ import styles from './FaqGroup.module.scss'
  */
 
 export default function FaqGroup({ items, variant = 'default' }) {
+
     const [openIndex, setOpenIndex] = useState(null)
 
-    const handleToggle = (index, e) => {
-        setOpenIndex(e.target.open ? index : null)
+    const handleToggle = (index) => {
+        setOpenIndex((currentIndex) =>
+            currentIndex === index ? null : index
+        )
     }
 
     const wrapperClass =
@@ -30,33 +32,45 @@ export default function FaqGroup({ items, variant = 'default' }) {
 
     return (
         <section className={wrapperClass}>
-            {items.map((item, i) => (
-                <details
-                    key={i}
-                    className={styles['faq-item']}
-                    open={openIndex === i}
-                    onToggle={(e) => handleToggle(i, e)}
-                >
-                    <summary className={styles['faq-summary']}>
-                        <span>{item.question}</span>
-                        <svg
-                            className={`${styles.icon}${openIndex === i ? ` ${styles.rotated}` : ''}`}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
+            {items.map((item, i) => {
+                const isOpen = openIndex === i
+
+                return (
+                    <details
+                        key={item.id ?? i}
+                        className={styles['faq-item']}
+                        open={isOpen}
+                    >
+                        <summary
+                            className={styles['faq-summary']}
+                            onClick={(event) => {
+                                event.preventDefault()
+                                handleToggle(i)
+                            }}
                         >
-                            <path
-                                fillRule="evenodd"
-                                d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 011.08 1.04l-4.25 4.25a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </summary>
-                    <article className={styles['faq-content']}>
-                        <p>{item.answer}</p>
-                    </article>
-                </details>
-            ))}
+                            <span>{item.question}</span>
+
+                            <svg
+                                className={`${styles.icon}${isOpen ? ` ${styles.rotated}` : ''}`}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 011.08 1.04l-4.25 4.25a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </summary>
+
+                        <article className={styles['faq-content']}>
+                            <p>{item.answer}</p>
+                        </article>
+                    </details>
+                )
+            })}
         </section>
     )
 }
